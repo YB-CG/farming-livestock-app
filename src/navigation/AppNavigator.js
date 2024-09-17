221989b2eebf188ebde35eaea569f04c312afb36
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-17 13:53:09 +0000

    navigator

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,65 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthContext } from '../contexts/AuthContext';
+
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import PersonalInfoScreen from '../screens/PersonalInfoScreen';
+import FarmInfoScreen from '../screens/FarmInfoScreen';
+import HomeScreen from '../screens/HomeScreen';
+import SplashScreen from '../screens/SplashScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import LivestockListScreen from '../screens/LivestockListScreen';
+import LivestockProfileScreen from '../screens/LivestockProfileScreen';
+import AddLivestockScreen from '../screens/AddLivestockScreen';
+import InventoryScreen from '../screens/InventoryScreen';
+
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  const { state } = useContext(AuthContext);
+
+  if (state.isLoading) {
+    return <SplashScreen />;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {state.userToken == null ? (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            {/* <Stack.Screen name="PersonalInfo" component={PersonalInfoScreen} />
+            <Stack.Screen name="FarmInfo" component={FarmInfoScreen} /> */}
+            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="Main" component={MainNavigator} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const MainStack = createStackNavigator();
+
+const MainNavigator = () => {
+  return (
+    <MainStack.Navigator screenOptions={{ headerShown: false }}>
+      <MainStack.Screen name="Home" component={HomeScreen} />
+      <MainStack.Screen name='Settings' component={SettingsScreen} />
+      <MainStack.Screen name='Livestock' component={LivestockListScreen} />
+      <MainStack.Screen name='LivestockProfile' component={LivestockProfileScreen} />
+      <MainStack.Screen name='AddLivestock' component={AddLivestockScreen} />
+      <MainStack.Screen name='Inventory' component={InventoryScreen} />
+      {/* <MainStack.Screen name="PersonalInfo" component={PersonalInfoScreen} /> */}
+      {/* <MainStack.Screen name="FarmInfo" component={FarmInfoScreen} /> */}
+    </MainStack.Navigator>
+  );
+};
+
+export default AppNavigator;
