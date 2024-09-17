4541f04a868b97df1a3dae690eafb7c61df46a2b
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-17 13:52:55 +0000

    reusable componehts

---
+++ src/components/ui/card/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export const Card = ({ children, style, ...props }) => (
+  <View style={[styles.card, style]} {...props}>
+    {children}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+    marginBottom: 16,
+  },
+});
+
+export default Card;
