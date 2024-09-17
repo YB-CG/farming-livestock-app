8857a3753875c1776d607aa56ce27d31eab62edd
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-17 13:52:06 +0000

    reusable componehts

---
+++ src/components/Loader.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+const Loader = ({ size = 'large', color = '#7CB342' }) => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size={size} color={color} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+  },
+});
+
+export default Loader;
